0243d2fc68c43da2a75886d3e1f9117e96f39225
Yasemin <ytanyu27@vt.edu>
2024-01-23 17:34:23 -0500

    Creation of model Class and fields

--- backend/plan_of_study/models.py
+++ backend/plan_of_study/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-# Create your models here.
-# change
+class Class(models.Model) :
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    major = models.CharField(max_length = 50)
+    # include number field
+    abbreviation = models.CharField(max_length = 50) # can we name this more specifically?
+    prereq = models.CharField(max_length = 150, blank = True)
+    term = models.CharField(max_length = 50, blank = True)
+    coreq = models.CharField(max_length = 150, blank = True)
+    description = models.TextField(blank = True)
+    credits = models.IntegerField()
+    editable_credits = models.BooleanField(default=False)
+    elective_field = models.JSONField(default = list)
+    elective_field_name = models.CharField(max_length = 50)
