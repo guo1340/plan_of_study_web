5d266ea300a50b483c2cf1c57a26f800e55aa029
PeiQing Guo <guo_peiqing5421@yahoo.com>
2024-02-05 12:47:03 -0500

    initial django model change

--- backend/classes/models.py
+++ backend/classes/models.py
@@ -1,1 +1,19 @@
+import uuid
+
 from django.db import models
+
+class Class(models.Model) :
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    major = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=150)
+    prereq = models.CharField(max_length=50)
+    term = models.CharField(max_length=50)
+    coreq = models.CharField(max_length=50)
+    description = models.TextField()
+    credits = models.IntegerField(default=0)
+    editable_credits = models.BooleanField(default=False)
+    elective_field = models.JSONField(default=list)
+    elective_field_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.abbreviation
--- backend/classes/views.py
+++ backend/classes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Class
+
 
 # Create your views here.
+def classes_home(request):
+    # classes = Class.object {"classes": classes}
+    return render(request, 'classes/list.html')
