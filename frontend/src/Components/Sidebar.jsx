8720f49bf7493cb2117c8d36db6a88eda3985c46
Yasemin <ytanyu27@vt.edu>
2024-02-05 13:01:17 -0500

    Finished the side bar component and added the corresponding style sheets in css

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,60 +1,76 @@
-import { BsHouseDoor, BsMap, BsBook, BsQuestionCircle, BsSearch} from "react-icons/bs";
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  BsHouseDoor,
+  BsMap,
+  BsBook,
+  BsQuestionCircle,
+  BsSearch,
+} from "react-icons/bs";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const barItem = [
+    {
+      path: "/",
+      name: "Home",
+      icon: <BsHouseDoor />,
+    },
 
-const Sidebar = ({children}) => {
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <BsMap />,
+    },
 
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () => setIsOpen(!isOpen)
-    const barItem = [
-        {
-            path: "/",
-            name: "Home",
-            icon: <BsHouseDoor />
-        }, 
+    {
+      path: "/courses",
+      name: "Courses",
+      icon: <BsBook />,
+    },
 
-        {
-            path: "/dashboard",
-            name: "Dashboard",
-            icon: <BsMap />
-        }, 
+    {
+      path: "/demos",
+      name: "Demos",
+      icon: <BsSearch />,
+    },
 
-        {
-            path: "/courses",
-            name: "Courses",
-            icon: <BsBook />
-        }, 
-
-        {
-            path: "/demos",
-            name: "Demos",
-            icon: <BsSearch />
-        },
-
-        {
-            path: "/faq",
-            name: "FAQ",
-            icon: <BsQuestionCircle />
-        }
-    ]
-    return (
-        <div className = "sidebar_container">
-            <div style = {{width: isOpen? "300px" : "100px"}} className = "sidebarStyle"> 
-            {
-                barItem.map((item, index) => (
-                    <NavLink to={item.path} key={index} className="link" activeclassname="active">
-                            <div className="icon">{item.icon}</div>
-                            <div style={{ display: isOpen ? "block" : "none" }} className="link_text">{item.name}</div>
-                    </NavLink>
-                ))
-
-            }
-
+    {
+      path: "/faq",
+      name: "FAQ",
+      icon: <BsQuestionCircle />,
+    },
+  ];
+  return (
+    <div className="sidebar_container">
+      <div
+        style={{ width: isOpen ? "300px" : "100px" }}
+        className="sidebarStyle"
+      >
+        {barItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassname="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
             </div>
-
-
+          </NavLink>
+        ))}
+        <div className="toggle_container">
+          <div className="extention_toggle" onClick={toggle}>
+            <AiOutlineMenu />
+          </div>
         </div>
-    )
-
-}
+      </div>
+      <main className="main_container">{children}</main>
+    </div>
+  );
+};
