0c317bc58b5c67cada0116e1c5df18908f3d0e24
PeiQing Guo <guo_peiqing5421@yahoo.com>
2024-11-07 13:56:26 -0500

    revert major object to original

--- backend/major/models.py
+++ backend/major/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Major(models.Model):
     name = models.CharField(max_length=100, default="")
-    abbreviation =\
-        models.CharField(max_length=10, default="")
+    abbreviation = models.CharField(max_length=10, default="")
 
     def __str__(self):
         return f"{self.abbreviation}':' {self.name}"
