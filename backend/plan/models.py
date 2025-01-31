59c05ec2fa2f71d607bfc127d5bce687d75b6b82
PeiQing Guo <guo_peiqing5421@yahoo.com>
2025-01-31 18:09:06 -0500

    added course cart to store newly added courses to plan before committing them to a specific semester

--- backend/plan/models.py
+++ backend/plan/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from template.models import Template
 from semester.models import Semester
+from classes.models import Course
 
 
 class Plan(models.Model):
@@ -8,6 +9,7 @@ class Plan(models.Model):
     name = models.CharField(max_length=100, default="")
     template = models.ForeignKey(Template, on_delete=models.PROTECT)
     semesters = models.ManyToManyField(Semester, blank=True)
+    course_cart = models.ManyToManyField(Course, blank=True)
 
     def __str__(self):
         return f"{self.template.major} plan: {self.name}"
