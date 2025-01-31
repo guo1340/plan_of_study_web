87dde54ebfca8082b9f42a9cf193330e16f523ab
PeiQing Guo <guo_peiqing5421@yahoo.com>
2025-01-31 18:08:18 -0500

    course object in semester is no longer required upon creation

--- backend/semester/models.py
+++ backend/semester/models.py
@@ -8,7 +8,7 @@ class Semester(models.Model):
     YEAR_CHOICES = [(r, r) for r in range(datetime.date.today().year, datetime.date.today().year + 10)]
     year = models.IntegerField(choices=YEAR_CHOICES, default=datetime.date.today().year)
     season = models.ForeignKey(Season, on_delete=models.PROTECT, default=1)
-    classes = models.ManyToManyField(Course)
+    classes = models.ManyToManyField(Course, blank=True)
     current_credit = models.IntegerField(default=0)
     max_credits = models.IntegerField(default=40)
     min_credits = models.IntegerField(default=0)
